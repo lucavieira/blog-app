dc95cca673eb838f47085d8307467e4589697f7f
lucavieira <lukasveras14@gmail.com>
2022-03-11 05:59:14 -0300

    Add categoria, edit categoria, validação de categoria e listagem de categoria. Além de suas respectivas rotas.

--- app.js
+++ app.js
@@ -5,9 +5,25 @@ const body_parser = require('body-parser')
 const app = express()
 const admin = require('./routes/admin')
 const path = require('path')
-//const mongoose = require('mongoose')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 // Config's
+    // Session
+        app.use(session({
+            secret: 'cursodenode',
+            resave: true,
+            saveUninitialized: true
+        }))
+
+        app.use(flash())
+    // Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash('success_msg')
+            res.locals.error_msg = req.flash('error_msg')
+            next()
+        })
     // body-parser
         app.use(body_parser.urlencoded({extended: true}))
         app.use(body_parser.json())
@@ -15,9 +31,17 @@ const path = require('path')
         app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
     // Mongoose
+        mongoose.connect('mongodb://localhost/blogapp').then(() => {
+            console.log('Conectado ao banco')
+        }).catch(error => {
+            console.log('Error ao se conectar. ' + error)
+        })
     // Public
         app.use(express.static(path.join(__dirname, 'public')))
 // Routes
+    app.get('/', (req, res) => {
+        res.send('Coming soon...')
+    })
     app.use('/admin', admin)
 // Others
 const PORT = 5000
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Category')
+const Categoria = mongoose.model('category')
 
 router.get('/', (req, res) => {
     res.render('admin/index')
@@ -10,7 +13,73 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/category', (req, res) => {
-    res.send('Category')
+    Categoria.find().lean().then(categorias => {
+        res.render('admin/category', {categorias: categorias})
+    }).catch(error => {
+        req.flash('error_msg', 'Erro ao listar categorias!')
+        res.redirect('/admin')
+    })
+})
+
+router.get('/category/add', (req, res) => {
+    res.render('admin/addcategory')
+})
+
+router.post('/category/new', (req, res) => {
+
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+        erros.push({error: 'Nome inválido.'})
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        erros.push({error: 'Slug inválido.'})
+    }
+
+    if(erros.length > 0) {
+        res.render('admin/addcategory', {erros: erros})
+    }else {
+        const newCategory = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+    
+        new Categoria(newCategory).save().then(() => {
+            req.flash('success_msg', 'Categoria criada com sucesso')
+            res.redirect('/admin/category')
+        }).catch(error => {
+            req.flash('error_msg', 'Falha ao criar a categoria. Tente novamente!')
+            res.redirect('/admin')
+        })
+    }
+})
+
+router.get('/category/edit/:id', (req, res) => {
+    Categoria.findOne({_id:req.params.id}).lean().then(categoria => {
+        res.render('admin/editcategory', {categoria: categoria})
+    }).catch(error => {
+        req.flash('error_msg', 'Categoria não encontrada.')
+        res.redirect('/admin/category')
+    })
+})
+
+router.post('/category/edit', (req, res) => {
+    Categoria.findOne({_id:req.body.id}).then(categoria => {
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+
+        categoria.save().then(() => {
+            req.flash('success_msg', 'Categoria editada.')
+            res.redirect('/admin/category')
+        }).catch(error => {
+            req.flash('error_msg', 'Error ao editar a categoria')
+            res.redirect('/admin/category')
+        })
+    }).catch(error => {
+        req.flash('error_msg', 'Erro ao editar a categoria')
+        res.redirect('/admin/category')
+    })
 })
 
 module.exports = router
