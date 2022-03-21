ac271f13d720d92d7356c45819ae5b6760cbf011
Lucas Vieira Veras <63080209+lucavieira@users.noreply.github.com>
2022-03-21 12:22:55 -0300

    Delete db.js

--- config/db.js
+++
@@ -1,6 +0,0 @@
-if(process.env.NODE_ENV == 'production') {
-    //teste
-    module.exports = {mongoURI: "mongodb+srv://adm_lucas:rTFi2Omsw0vizq5n@blogapp.lsxcb.mongodb.net/blogapp?retryWrites=true&w=majority"}
-} else {
-    module.exports = {mongoURI: "mongodb://localhost/blogapp"}
-}
