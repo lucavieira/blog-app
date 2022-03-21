4074895256c46d696dacad646061550a7ce8c47a
lucavieira <lukasveras14@gmail.com>
2022-03-21 12:16:56 -0300

    security

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 if(process.env.NODE_ENV == 'production') {
+    //teste
     module.exports = {mongoURI: "mongodb+srv://adm_lucas:rTFi2Omsw0vizq5n@blogapp.lsxcb.mongodb.net/blogapp?retryWrites=true&w=majority"}
 } else {
     module.exports = {mongoURI: "mongodb://localhost/blogapp"}
